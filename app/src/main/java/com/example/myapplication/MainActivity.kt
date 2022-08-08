8fe9de32c7c08bcbcd5ca6e59c71459bec8105e0
Artem <sayfutdinov.a@inbox.ru>
2022-08-08 20:13:51 +0300

    New Branch commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,44 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+    private var data: Float = 0f
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding?.root
+        setContentView(view)
+        if (savedInstanceState != null) {
+            dataBinding(savedInstanceState?.getFloat(DATA_KEY))
+        } else {
+            dataBinding(0f)
+        }
+        binding?.slider?.addOnChangeListener { slider, value, fromUser ->
+            dataBinding(value)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putFloat(DATA_KEY, data)
+    }
+
+    private fun dataBinding(dataBinding: Float) {
+        binding?.textRes?.text = "Your data is ${dataBinding}"
+        binding?.slider?.value = dataBinding
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+
+    companion object {
+        const val DATA_KEY = "123" //ключ Bundle
     }
 }
